b458275fe5a437d897ac7fbba7b8a360013da64b
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-08-15 01:43:13 +0600

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import LoadingScreen from './components/LoadingScreen'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ isLoaded , setIsLoaded ] = useState(false)
 
   return (
+    
     <>
-    portfolio
+     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
   )
 }
